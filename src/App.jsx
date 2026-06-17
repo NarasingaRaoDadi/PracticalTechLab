69b27b74271562997ba9654f15cb5b56be401250
NarasingaRaoDadi <sr19470001@gmail.com>
2026-06-17 12:56:05 +0530

    Hero section created

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div>
+      <section>
+        <h1>LexAutomation</h1>
+        <h2>Legal Technology Solutions</h2>
+
+        <p>
+          Bridging Law and Technology through innovative legal automation,
+          software solutions, and digital transformation.
+        </p>
+
+        <button>Contact Us</button>
+      </section>
+    </div>
+  );
+}
+
+export default App;
